b0d57878e7abb5e0353a15761136f3f26e03fe45
wb1110 <williamjaredbuchanan@gmail.com>
2023-03-01 16:30:28 -0600

    second page added

---
+++ volleytech/pages/staffLogin.tsx
@@ -0,0 +1,12 @@
+import styles from '@/styles/Home.module.css'
+import Head from 'next/head'
+
+export default function StaffLogin() {
+  return (
+    <div>
+      <main className={styles.main}>
+    <h1>Staff Login</h1>
+      </main>
+    </div>
+  )
+}
